3cc0968ea591d52ed0a9c08ceba023ade20fda78
gautamsood15 <gauty22@gmail.com>
2019-12-30 08:06:01 +0530

    Adding code for main buttons to execute + hiding tab bar

--- index.py
+++ index.py
@@ -20,17 +20,50 @@ class MainApp(QMainWindow , ui):
     
     def Handel_UI_Changes(self):
         self.Hiding_Themes()
+        self.tabWidget.tabBar().setVisible(False)
         
     
     def Handel_Buttons(self):
         self.pushButton_5.clicked.connect(self.Show_Themes)
         self.pushButton_21.clicked.connect(self.Hiding_Themes)
 
+        self.pushButton.clicked.connect(self.Open_Today_Tab)
+        self.pushButton_2.clicked.connect(self.Open_Books_Tab)
+        self.pushButton_3.clicked.connect(self.Open_Users_tab)
+        self.pushButton_4.clicked.connect(self.Open_Settings_Tab)
+
+
+
+
     def Show_Themes(self):
         self.groupBox_3.show()
 
     def Hiding_Themes(self):
         self.groupBox_3.hide()
+
+
+###################### OPENING TABS ####################################
+
+    def Open_Today_Tab(self):
+        self.tabWidget.setCurrentIndex(0)
+
+    def Open_Books_Tab(self):
+        self.tabWidget.setCurrentIndex(1)
+
+    def Open_Users_tab(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def Open_Settings_Tab(self):
+        self.tabWidget.setCurrentIndex(3)
+
+
+
+
+
+
+
+
+
 
 
 
