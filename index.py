93aa2d85f26891aa072fe6018f52d75614d57a3d
gautamsood15 <gauty22@gmail.com>
2019-12-30 11:08:46 +0530

    Settings Tab code added

--- index.py
+++ index.py
@@ -18,7 +18,8 @@ class MainApp(QMainWindow , ui):
         self.Handel_Buttons()
 
         self.Show_Category()
-
+        self.Show_Author()
+        self.Show_Publisher()
     
     def Handel_UI_Changes(self):
         self.Hiding_Themes()
@@ -135,7 +136,6 @@ class MainApp(QMainWindow , ui):
 
         self.cur.execute( ''' SELECT category_name FROM category ''')
         data = self.cur.fetchall()
-        print(data)
 
         if data :
             self.tableWidget_2.setRowCount(0)
@@ -167,13 +167,27 @@ class MainApp(QMainWindow , ui):
         self.db.commit()
         self.statusBar().showMessage('New Author Added')
         self.lineEdit_23.setText('')
-
+        self.Show_Author()
 
 
     def Show_Author(self):
 
         self.db = MySQLdb.connect(host='localhost' , user='root' , password='root@123' , db='library')
         self.cur = self.db.cursor()
+
+        self.cur.execute( ''' SELECT author_name FROM author ''')
+        data = self.cur.fetchall()
+
+        if data :
+            self.tableWidget_3.setRowCount(0)
+            self.tableWidget_3.insertRow(0)
+            for row , form in enumerate(data):
+                for column , item in enumerate(form):
+                    self.tableWidget_3.setItem(row , column , QTableWidgetItem(str(item)))
+                    column += 1
+
+                row_position = self.tableWidget_3.rowCount()
+                self.tableWidget_3.insertRow(row_position)
 
 
 
@@ -194,12 +208,28 @@ class MainApp(QMainWindow , ui):
         self.db.commit()
         self.statusBar().showMessage('New Publisher Added')
         self.lineEdit_24.setText('')
-
+        self.Show_Publisher()
 
 
 
     def Show_Publisher(self):
-        pass
+
+        self.db = MySQLdb.connect(host='localhost' , user='root' , password='root@123' , db='library')
+        self.cur = self.db.cursor()
+
+        self.cur.execute( ''' SELECT publisher_name FROM publisher ''')
+        data = self.cur.fetchall()
+
+        if data :
+            self.tableWidget_4.setRowCount(0)
+            self.tableWidget_4.insertRow(0)
+            for row , form in enumerate(data):
+                for column , item in enumerate(form):
+                    self.tableWidget_4.setItem(row , column , QTableWidgetItem(str(item)))
+                    column += 1
+
+                row_position = self.tableWidget_4.rowCount()
+                self.tableWidget_4.insertRow(row_position)
 
 
 
